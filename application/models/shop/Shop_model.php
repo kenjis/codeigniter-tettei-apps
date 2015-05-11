bb9bdfed17e74203f7409bef0c5e54e9ccecc3d8
kenjis <kenji.uui@gmail.com>
2015-05-11 20:13:05 +0900

    リファクタリング：メール本文の作成をテンプレートパーサクラスを使うように変更

--- application/models/shop/Shop_model.php
+++ application/models/shop/Shop_model.php
@@ -19,72 +19,33 @@ class Shop_model extends CI_Model {
 	public function order()
 	{
 # 注文日時をPHPのdate()関数から取得します。
-		$date = date("Y/m/d H:i:s");
+		$data['date'] = date("Y/m/d H:i:s");
 # カートの情報を取得します。
 		$cart = $this->Cart_model->get_all();
-		$total = number_format($cart['total']);
+		foreach ($cart['items'] as &$item)
+		{
+			$item['price']  = number_format($item['price']);
+			$item['amount'] = number_format($item['amount']);
+		}
+		$data['items']  = $cart['items'];
+		$data['line']  = $cart['line'];
+		$data['total'] = number_format($cart['total']);
 
 # お客様情報を取得します。
-		$data = $this->Customer_model->get();
-		$name  = $data['name'];
-		$zip   = $data['zip'];
-		$addr  = $data['addr'];
-		$tel   = $data['tel'];
-		$email = $data['email'];
+		$data = array_merge($data, $this->Customer_model->get());
 
 # メールのヘッダを設定します。Bccで同じメールを管理者にも送るようにします。
 		$mail['from_name'] = 'CIショップ';
 		$mail['from']      = $this->admin;
-		$mail['to']        = $email;
+		$mail['to']        = $data['email'];
 		$mail['bcc']       = $this->admin;
 		$mail['subject']   = '【注文メール】CIショップ';
 
-# ヒアドキュメントでメール本文を作成します。
-		$mail['body'] = <<< END
-CIショップにご注文いただきありがとうございます。ご注文内容は以下のとおりです。
-
-***********************************************************
- お名前とご請求金額
-***********************************************************
-       お名前: $name
-メールアドレス: $email
-
-     注文合計： {$total}円
-
-***********************************************************
- 注文内容
-***********************************************************
-注文日時: $date
-
-
-END;
-
-		foreach ($cart['items'] as $line => $item)
-		{
-			$mail['body'] .= $item['name'] . "\n";
-			$mail['body'] .= '数量: ' . $item['qty'] . "\n";
-			$mail['body'] .= '単価: ' . number_format($item['price']) . "円\n";
-			$mail['body'] .= '金額: ' . number_format($item['amount']) . "円\n\n";
-		}
-		$mail['body'] .= '合計金額: ' . $total ."円\n\n";
-
-		$mail['body'] .= <<< END
-***********************************************************
- お届け先
-***********************************************************
-$zip
-$addr
-$name
-$tel
-
-***********************************************************
-今後ともCIショップをよろしくお願いいたします。
-------------------------------------------------------------- 
-CIショップ
-
-http://codeigniter.jp
-------------------------------------------------------------- 
-END;
+# テンプレートパーサクラスでメール本文を作成します。
+		$this->load->library('parser');
+		$mail['body'] = $this->parser->parse(
+			'templates/mail/shop_order', $data, TRUE
+		);
 
 # sendmail()メソッドを呼び出し、実際にメールを送信します。メール送信に成功
 # すれば、TRUEを返します。
