20105c91b4a33026a39e291dd32838e51b652710
kenjis <kenji.uui@gmail.com>
2015-05-11 15:17:29 +0900

    テストの追加、カバー率計算の修正
    カバー率 57.60% (481 / 835)

--- application/tests/controllers/Bbs_test.php
+++ application/tests/controllers/Bbs_test.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\DomCrawler\Crawler;
+
 class Bbs_test extends TestCase
 {
 	public function setUp()
@@ -13,6 +15,24 @@ class Bbs_test extends TestCase
 	{
 		$output = $this->request('GET', ['bbs', 'index'], [], $this->load_agent);
 		$this->assertContains('<title>掲示板</title>', $output);
+	}
+
+	public function test_index_mobile()
+	{
+		$agent = $this->getMockBuilder('CI_User_agent')
+			->getMock();
+		$agent->method('is_mobile')
+			->willReturn(true);
+
+		get_new_instance();
+		$obj = new Bbs();
+		$obj->agent = $agent;
+
+		ob_start();
+		$obj->index();
+		$output = ob_get_clean();
+
+		$this->assertContains('<title>ﾓﾊﾞｲﾙ掲示板</title>', $output);
 	}
 
 	public function test_post()
@@ -75,5 +95,78 @@ class Bbs_test extends TestCase
 
 		$output = $this->request('GET', ['bbs', 'index'], [], $this->load_agent);
 		$this->assertContains(html_escape($subject), $output);
+	}
+
+	public function test_delete()
+	{
+		// Warningを抑制する
+		// Severity: WarningMessage:  Cannot modify header information - headers already sent
+		$er = error_reporting(E_ALL & ~E_WARNING);
+		
+		$output = $this->request(
+			'POST',
+			['bbs', 'insert'],
+			[
+				'name' => "削除太郎",
+				'email' => "test@example.jp",
+				'subject' => "削除する投稿",
+				'body' => "この投稿を削除します。",
+				'password' => "delete",
+				'captcha' => "8888",
+				'key' => "139",
+			],
+			$this->load_agent
+		);
+		
+		// error_reportingを戻す
+		error_reporting($er);
+
+		$output = $this->request('GET', ['bbs', 'index'], [], $this->load_agent);
+		$crawler = new Crawler($output);
+		
+		// 最初の <h1><a>〜</a></h1> のテキストを取得
+		$text = $crawler->filter('h1 > a')->eq(0)->text();
+		$id = trim($text, '[]');
+		
+		$output = $this->request('POST', ['bbs', 'delete', $id], [], $this->load_agent);
+		$this->assertContains('記事を削除できませんでした', $output);
+
+		$output = $this->request(
+			'POST',
+			['bbs', 'delete', $id],
+			['password' => 'delete'],
+			$this->load_agent
+		);
+		$this->assertContains('削除の確認', $output);
+
+		$output = $this->request(
+			'POST',
+			['bbs', 'delete', $id],
+			[
+				'password' => 'bad password',
+				'delete' => '1',
+			],
+			$this->load_agent
+		);
+		$this->assertContains('記事を削除できませんでした', $output);
+
+		$output = $this->request(
+			'POST',
+			['bbs', 'delete', $id],
+			[
+				'password' => 'delete',
+				'delete' => '1',
+			],
+			$this->load_agent
+		);
+		$this->assertContains('記事の削除完了', $output);
+	}
+
+	public function test_captcha_check()
+	{
+		$obj = new Bbs();
+		$obj->form_validation = new CI_Form_validation();
+		$actual = $obj->captcha_check('bad_input');
+		$this->assertFalse($actual);
 	}
 }
