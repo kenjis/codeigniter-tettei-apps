c5ea3e82c55a3d395865bd882e36eacf3c2366f1
kenjis <kenji.uui@gmail.com>
2015-05-13 11:34:22 +0900

    必要なくなったので permitted_uri_chars をデフォルトに戻す

--- application/config/config.php
+++ application/config/config.php
@@ -153,7 +153,7 @@ $config['composer_autoload'] = realpath(APPPATH . '../vendor/autoload.php');
 | DO NOT CHANGE THIS UNLESS YOU FULLY UNDERSTAND THE REPERCUSSIONS!!
 |
 */
-$config['permitted_uri_chars'] = '一-龠ぁ-んァ-ヴーａ-ｚＡ-Ｚ０-９a-z 0-9~%.:_-';
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
 
 
 /*
