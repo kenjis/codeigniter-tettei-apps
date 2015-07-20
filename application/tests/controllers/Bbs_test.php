721cc1a68a558f5bdceab268d5d1bc15cca9d947
kenjis <kenji.uui@gmail.com>
2015-07-20 09:20:35 +0900

    ci-phpunit-testのアップデートに伴うテストの更新

--- application/tests/controllers/Bbs_test.php
+++ application/tests/controllers/Bbs_test.php
@@ -68,28 +68,21 @@ class Bbs_test extends TestCase
 
 	public function test_insert()
 	{
-		try
-		{
-			$subject = "<s>xyz</s> " . time();
-			$output = $this->request(
-				'POST',
-				['bbs', 'insert'],
-				[
-					'name' => "<s>xyz</s>",
-					'email' => "test@example.jp",
-					'subject' => $subject,
-					'body' => "<s>xyz</s>",
-					'password' => "<s>xyz</s>",
-					'captcha' => "8888",
-					'key' => "139",
-				]
-			);
-		}
-		catch (PHPUnit_Framework_Exception $e)
-		{
-			$this->assertEquals(302, $e->getCode());
-			$this->assertRegExp('!\ARedirect to .+/bbs\z!', $e->getMessage());
-		}
+		$subject = "<s>xyz</s> " . time();
+		$output = $this->request(
+			'POST',
+			['bbs', 'insert'],
+			[
+				'name' => "<s>xyz</s>",
+				'email' => "test@example.jp",
+				'subject' => $subject,
+				'body' => "<s>xyz</s>",
+				'password' => "<s>xyz</s>",
+				'captcha' => "8888",
+				'key' => "139",
+			]
+		);
+		$this->assertRedirect('bbs', 302);
 
 		$output = $this->request('GET', ['bbs', 'index']);
 		$this->assertContains(html_escape($subject), $output);
