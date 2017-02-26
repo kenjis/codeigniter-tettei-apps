b0448d186fe48e0bfbc2c75c4dc9ab78b85958fd
kenjis <kenji.uui@gmail.com>
2017-02-26 09:44:56 +0900

    systemフォルダの場所変更に伴うパスの更新

--- application/tests/Bootstrap.php
+++ application/tests/Bootstrap.php
@@ -102,7 +102,7 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = '../../system';
+	$system_path = '../../vendor/codeigniter/framework/system';
 
 /*
  *---------------------------------------------------------------
@@ -117,7 +117,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = '../application';
+	$application_folder = '../../application';
 
 /*
  *---------------------------------------------------------------
