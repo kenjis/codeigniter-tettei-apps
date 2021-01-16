171775f42edc8c0dcd81fcaf8be8a96e29cc2f54
kenjis <kenji.uui@gmail.com>
2021-01-16 10:28:15 +0900

    docs: PHPビルトインサーバを使う場合の base_url をコメントとして追加

--- application/config/config.php
+++ application/config/config.php
@@ -18,6 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/CodeIgniter/';
+//$config['base_url'] = 'http://localhost:8000/';
 
 /*
 |--------------------------------------------------------------------------
